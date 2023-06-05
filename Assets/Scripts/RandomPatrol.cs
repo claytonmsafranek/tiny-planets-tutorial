91d942ff5b4af56b3cd0b0449ba67a936c043883
Clayton Safranek <claytonsafranek@gmail.com>
2023-06-05 18:21:03 -0500

    planets moving

--- Assets/Scripts/RandomPatrol.cs
+++ Assets/Scripts/RandomPatrol.cs
@@ -9,6 +9,10 @@ public class RandomPatrol : MonoBehaviour
     public float minY;
     public float maxY;
 
+    public float speed;
+
+    Vector2 targetPosition;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,11 +23,32 @@ public class RandomPatrol : MonoBehaviour
         minY = ScreenUtils.ScreenBottom;
         maxY = ScreenUtils.ScreenTop;
 
+        // define target for planets to move to
+        targetPosition = GetRandomPosition();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // check if planet position is equal to target position, need to cast transform to vector2 from vector3
+        if ((Vector2)transform.position != targetPosition)
+        {
+            // MoveTowards takes in current position, target postion, and speed to move
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            // calcualte a new target position
+            targetPosition = GetRandomPosition();
+        }
         
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX, randomY);
     }
 }
